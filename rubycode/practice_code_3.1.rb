94fc20d21d169bd6460f114f431321843c9e2290
sajidali36 <sajidali.362537@gmail.com>
2022-04-12 14:26:14 +0500

    task1 updated

--- rubycode/practice_code_3.1.rb
+++ rubycode/practice_code_3.1.rb
@@ -1,16 +1,16 @@
-class Countfrequency
-  def computefrequency=(arr)
-    dict = {}
-    dict.default = 0
-    arr.each do |i|
-      dict[i] += 1
+class CountFrequency
+  def compute_frequency(array)
+    dictionary = {}
+    dictionary.default = 0
+    array.each do |i|
+      dictionary[i] += 1
     end
-    dict.each do |x,y|
+    dictionary.each do |x,y|
       puts "#{x}=>#{y}"
     end
   end
 end
 
-arr = [1, 2, 3, 4, 1, 1, 1, 2, 3, 11, 11, 2, 33]
-obj = Countfrequency.new
-obj.computefrequency = arr
+array = [1, 2, 3, 4, 1, 1, 1, 2, 3, 11, 11, 2, 33]
+obj = CountFrequency.new
+obj.compute_frequency(array)
